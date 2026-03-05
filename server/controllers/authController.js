9e6e3645d3926ab14fe9c228c373c43d4eaed996
lwazi7373 <lwazincube7373@gmail.com>
2026-03-05 18:10:23 +0200

    added register and login controllers

---
+++ server/controllers/authController.js
@@ -0,0 +1,59 @@
+const bcrypt = require("bcrypt"); //encrypt passwords
+const jwt = require("jsonwebtoken"); //create tokens
+const { unauthorized } = require("../errors/httpErrors");
+const authService = require("../services/authService");
+
+/**
+ * Controller to register the user to the system
+ * @param {*} req
+ * @param {*} res
+ * @returns
+ */
+const register = async (req, res) => {
+  const { userName, userPassword, emailAddress, contactNo, isActive } = req.body;
+
+  // Hash the password
+  const salt = bcrypt.genSaltSync(10);
+  const hashedPassword = bcrypt.hashSync(userPassword, salt);
+  
+  // Register the user
+  const newUserId = await authService.registerUser(
+    userName,
+    hashedPassword,
+    emailAddress,
+    contactNo,
+    isActive,
+  );
+
+  // Send response
+  res.status(200).json({msg: `Sucessfully registered user: ${newUserId}`});
+};
+
+
+/**
+ * Verifies user credentials, if successful creates an authToken for the user and logs them in
+ * authToken expires after 1 day, that is 24 hours
+ * @param {String} req
+ * @param {String} res
+ * @returns 
+ */
+const login = async (req, res) => {
+  const { userName, userPassword } = req.body;
+  const user = await authService.loginUser(userName); 
+
+  // After getting the user object, validate if the password is correct
+  const isPasswordValid = await bcrypt.compare(userPassword, user.userPassword);
+
+  //No seperation for security purposes, to avoid giving out too much information.
+  if (!user || !isPasswordValid) throw unauthorized("Invalid userName or userPassword");
+
+  // If valid, then create token
+  const payload = { userId: user.userId, userName: user.userName};
+  const secret = process.env.JWT_SECRET;
+  const authToken = jwt.sign(payload, secret, { expiresIn: "1d" });
+
+  // Send response
+  res.status(200).json({msg: "Sucessfully logged In User", authToken});
+};
+
+module.exports = {register, login};
