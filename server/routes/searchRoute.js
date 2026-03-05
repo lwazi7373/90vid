34eb3e198b1265717da84b6606920f7981b6d4c0
lwazi7373 <lwazincube7373@gmail.com>
2026-03-05 18:46:53 +0200

    updated searchRoutes

--- server/routes/searchRoute.js
+++ server/routes/searchRoute.js
@@ -7,7 +7,7 @@ const authenticateToken = require("../middleware/authMiddleware");
 
 /**
  * GET /api/rooms?roomName=football
- * Search for the specified room
+ * Find the room the user is searching for  
  * Note rooms can have the same name, that is roomName is not unique in database (So return might be more than one room)
  */
 router.get("/search/rooms", authenticateToken, searchController.searchRoom);
