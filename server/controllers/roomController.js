f849777dca871143790b7668490dac21643c0763
lwazi7373 <lwazincube7373@gmail.com>
2026-03-12 11:06:41 +0200

    added complete room controllers

---
+++ server/controllers/roomController.js
@@ -0,0 +1,56 @@
+const roomService = require("../services/roomService");
+
+const getRooms = async (req, res, next) => {
+  try {
+    const rooms = await roomService.getRooms();
+    return res.status(200).json({ results: rooms, count: rooms.length });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getRoom = async (req, res, next) => {
+  try {
+    const { roomId } = req.params; 
+    const room = await roomService.getRoom(roomId);
+    return res.status(200).json(room);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const createRoom = async (req, res, next) => {
+  try {
+    const userId = req.user.userId;
+    const { roomName, description } = req.body;
+    const room = await roomService.createRoom(userId, { roomName, description });
+    return res.status(201).json(room);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateRoom = async (req, res, next) => {
+  try {
+    const userId = req.user.userId;
+    const { roomId } = req.params;
+    const { roomName, description } = req.body;
+    const room = await roomService.updateRoom(roomId, userId, { roomName, description });
+    return res.status(200).json(room);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const deleteRoom = async (req, res, next) => {
+  try {
+    const userId = req.user.userId;
+    const { roomId } = req.params;
+    await roomService.deleteRoom(roomId, userId);
+    return res.status(200).json({ message: "Room deleted successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { getRooms, getRoom, createRoom, updateRoom, deleteRoom };
