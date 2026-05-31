8aa41d29aa1fc4b79ab742bbca06d69227147df5
lwazi7373 <lwazincube7373@gmail.com>
2026-05-31 10:37:56 +0200

    updated routes

--- client/src/routes/appRoutes.tsx
+++ client/src/routes/appRoutes.tsx
@@ -1,6 +1,10 @@
 import { Routes, Route} from "react-router-dom";
 import RegisterPage from "../features/auth/pages/RegisterPage";
 import LoginPage from "../features/auth/pages/LoginPage";
+import { AllRooms } from "../features/room/pages/AllRooms";
+import { UsersRooms } from "../features/room/pages/UsersRooms";
+import { PermittedRooms } from "../features/room/pages/PermittedRooms";
+import { RoomView } from "../features/room/pages/RoomView";
 
 function AppRoutes() {
     return(
@@ -11,6 +15,10 @@ function AppRoutes() {
         {/* User Routes */}
         {/* Search Routes */}
         {/* Room Routes */}
+        <Route path="/rooms" element={<AllRooms />} />
+        <Route path="/rooms/mine" element={<UsersRooms />} />
+        <Route path="/rooms/permitted" element={<PermittedRooms />} />
+        <Route path="/rooms/:roomId" element={<RoomView />} />
         {/* Media Routes */}
         {/* Permission Routes */}
         </Routes>
